289eab7ecbd2753ce5c79415855ca05f4887e559
Haza Jijan <hazajijan@gmail.com>
2026-04-28 14:31:12 +0200

    feat(frontend): add initial Streamlit app shell

---
+++ src/frontend/app.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+import httpx
+import os 
+
+API_URL = os.getenv("API_URL","http://localhost:8000")
+
+def layout():
+    st.markdown("# 404 brain not found ")
+    st.markdown("Ask me about onboarding")
+    
+    text_input = st.text_input(label="Ask a question")
+    
+    if st.button("send") and text_input.strip() != "":
+        response = httpx.post(
+            f"{API_URL}/rag/query", json={"prompt": text_input}, timeout=120
+        )
+        
+        data = response.json()
+        
+        st.markdown("## Question")
+        st.markdown(text_input)
+        
+        st.markdown("## Answer")
+        st.markdown(data.get("answer"))
+        
+        st.markdown("## Source")
+        st.markdown(data.get("filename"))
+
+
+if __name__ == "__main__":
+    layout()
+        
+    
