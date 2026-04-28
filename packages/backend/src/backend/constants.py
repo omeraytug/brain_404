37706931c4ac936da6869fe4ebda2770b12f4191
Omer Aytug <aytugomer00@gmail.com>
2026-04-29 00:20:16 +0200

    fix: base path redefined

--- packages/backend/src/backend/constants.py
+++ packages/backend/src/backend/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_PATH = Path(__file__).parents[1].resolve()
+BASE_PATH = Path(__file__).parents[3].resolve()
 
 DATA_PATH = BASE_PATH / "rag" / "data"
 VECTOR_DB_PATH = BASE_PATH / "rag" / "knowledge_base"
@@ -12,3 +12,5 @@ EMBEDDING_MODEL = "embed-multilingual-light-v3.0"
 MODEL_SMALL = "openrouter:openai/gpt-oss-20b:free"
 MODEL_MEDIUM = "google-gla:gemini-3-flash-preview"
 MODEL_LARGE = "openrouter:nvidia/nemotron-3-super-120b-a12b:free"
+
+
